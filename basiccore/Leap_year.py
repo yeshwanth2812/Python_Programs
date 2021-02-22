248047a7724b9481a830f5daf442ea5c9cd9fa38
yeshwanth281297 <yesh281297@gmail.com>
2021-02-22 23:20:36 +0530

    ADDED:Leap Year

---
+++ basiccore/Leap_year.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 13:00:53
+# @Title: Leap year
+
+
+year = 2021       #take value from user and try catch block missing
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a not leap year".format(year)) 
+else:
+   print("{0} is not a leap year".format(year))
