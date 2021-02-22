d74177b5b312ef641e28bad91f673ad524c60e93
yeshwanth281297 <yesh281297@gmail.com>
2021-02-22 23:24:30 +0530

    ADDED: Cubic

---
+++ functional/Cubic.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 13:00:53
+# @Title:program to find a triplet that sum to a given value 
+# returns true if there is triplet with 
+# sum equal to 'sum' present in A[].  "doc string and try catch exception need to be implemented"
+
+def find3Numbers(A, arr_size, sum): 
+  
+    # Fix the first element as A[i] 
+    for i in range( 0, arr_size-2): 
+  
+        # Fix the second element as A[j] 
+        for j in range(i + 1, arr_size-1):  
+              
+            # Now look for the third number 
+            for k in range(j + 1, arr_size): 
+                if A[i] + A[j] + A[k] == sum: 
+                    print("Triplet is", A[i], 
+                          ", ", A[j], ", ", A[k]) 
+                    return True
+      
+    # If we reach here, then no  
+    # triplet was found 
+    return False
+  
+# Driver program to test above function  
+A = [1, 4, 45, 6, 10, 8] 
+sum = 22
+arr_size = len(A) 
+find3Numbers(A, arr_size, sum) 
