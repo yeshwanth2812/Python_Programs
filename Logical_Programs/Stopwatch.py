c4397d73099f32c9ebd03008a3f27ac3814f025a
yeshwanth281297 <yesh281297@gmail.com>
2021-02-22 23:29:42 +0530

    ADDED:Stop Watch

---
+++ Logical_Programs/Stopwatch.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: Yeshwanth
+# @Date: 2021-01-04 18:58:12
+# @Last Modified by: Yeshwanth
+# @Last Modified time: 2021-01-09 11:59:53
+# @title :Stop Watch
+import time
+
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+
+input("Press Enter to start")
+start_time = time.time()
+
+input("Press Enter to stop")
+end_time = time.time()
+
+time_lapsed = end_time - start_time
+time_convert(time_lapsed)
